38451ec8c667215be2337fa62b8cd3450cf7b5ca
deepikabalan12345 <157381325+deepikabalan12345@users.noreply.github.com>
2024-07-15 16:09:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function ispalindrome (str){
+    const rs=str.split('').reverse().join('');
+    if (str==rs){
+    console.log(`The given string "${str}" is a palindrome`);
+    }
+    else{ 
+        console.log(`the given string "${str}" is not a palindrome`);
+            }
+}
+(ispalindrome("malayalam"));
+(ispalindrome("hello"));
+
